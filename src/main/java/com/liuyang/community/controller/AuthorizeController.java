8a63620dbef7d05a585cf6a2b580f4df49a5362b
seniYangL <18754003009@163.com>
2019-10-12 22:12:10 +0800

    1.提取出github可变参数

--- src/main/java/com/liuyang/community/controller/AuthorizeController.java
+++ src/main/java/com/liuyang/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.liuyang.community.dto.AccessTokenDTO;
 import com.liuyang.community.dto.GitHubUser;
 import com.liuyang.community.provider.GitHubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,14 +15,21 @@ public class AuthorizeController {
     @Autowired
     private GitHubProvider gitHubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("0ed4c4045952dd357c6d");
-        accessTokenDTO.setClient_secret("dc26487466226d0220429278fedf83f8307193da");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
         GitHubUser user = gitHubProvider.getUser(accessToken);
